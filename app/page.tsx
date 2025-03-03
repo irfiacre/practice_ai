c12449b2f0909d9c34087429f63807a3a427ff37
irfiacre <irfiacre@gmail.com>
2025-03-03 21:06:15 +0200

    Improve footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 "use client";
+import Footer from "@/src/components/Footer";
 import { useState } from "react";
 
 export default function Home() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState<unknown>(null);
 
-  console.log("----------", state);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <span>
-          Header
-        </span>
+        <span>Header</span>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <span>xxx</span>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <span>xxx | yyy</span>
+    </footer>
+  );
+};
+
+export default Footer;
