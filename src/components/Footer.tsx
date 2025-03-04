31269dbb156e97561ddf127f647a56139a3df324
irfiacre <irfiacre@gmail.com>
2025-03-04 23:21:01 +0200

    Improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <span>xxx | yyy</span>
+      <span>xxx</span>
     </footer>
   );
 };
